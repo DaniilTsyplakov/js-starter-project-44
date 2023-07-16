8cec2c9c6a2285a8de153e7f1371e997bedf42ed
Daniil Tsyplakov <tsyplakovdan@gmail.com>
2023-07-16 13:06:30 +0800

    add brain-progressive to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 import playBrainCalc from '../games/brain-calc.js';
 import playBrainEven from '../games/brain-even.js';
 import playBrainGCD from '../games/brain-gcd.js';
+import playBrainProgression from '../games/brain-progression.js';
 
 const runGame = (gameName) => {
   switch (gameName) {
@@ -14,6 +15,9 @@ const runGame = (gameName) => {
     case '3':
       playBrainGCD();
       break;
+    case '4':
+      playBrainProgression();
+      break;
     default:
       throw new Error(`Unknown game: ${gameName}`);
   }
@@ -22,5 +26,6 @@ console.log('Choose game:');
 console.log('1. Brain Even');
 console.log('2. Brain Calc');
 console.log('3. Brain GCD');
+console.log('4. Brain Progressive');
 const choice = readlineSync.question('Enter the number of the game: ');
 runGame(choice);
