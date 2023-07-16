08564ba67a9fcdfb809b1bd52e48410aff904a33
Daniil Tsyplakov <tsyplakovdan@gmail.com>
2023-07-16 14:24:03 +0800

    add brain-prime to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import playBrainCalc from '../games/brain-calc.js';
 import playBrainEven from '../games/brain-even.js';
 import playBrainGCD from '../games/brain-gcd.js';
 import playBrainProgression from '../games/brain-progression.js';
+import playBrainPrime from '../games/brain-prime.js';
 
 const runGame = (gameName) => {
   switch (gameName) {
@@ -18,8 +19,11 @@ const runGame = (gameName) => {
     case '4':
       playBrainProgression();
       break;
+    case '5':
+      playBrainPrime();
+      break;
     default:
-      throw new Error(`Unknown game: ${gameName}`);
+      console.log(`Unknown game: ${gameName}`);
   }
 };
 console.log('Choose game:');
@@ -27,5 +31,6 @@ console.log('1. Brain Even');
 console.log('2. Brain Calc');
 console.log('3. Brain GCD');
 console.log('4. Brain Progressive');
+console.log('5. Brain Prime');
 const choice = readlineSync.question('Enter the number of the game: ');
 runGame(choice);
